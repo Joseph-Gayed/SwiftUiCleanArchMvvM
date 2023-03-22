f9bf98f8c2782c4ef312af7252ff5b51a0e79868
Jo <josephfathy1991@gmail.com>
2023-03-22 20:14:18 +0200

    Initial Commit

---
+++ MvvMExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MvvMExample
+//
+//  Created by Joseph Gayed on 22/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MvvMExample/MvvMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MvvMExampleApp.swift
+//  MvvMExample
+//
+//  Created by Joseph Gayed on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MvvMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
